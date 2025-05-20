575736b0eee677156d4d37c92d3a9af97586bc64
woobertdoo <jamie.a.harris97@gmail.com>
2025-05-19 20:56:34 -0500

    React Framework for DotA Quiz

---
+++ src/routes/Logout.js
@@ -0,0 +1,23 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+function Logout(props) {
+  const { handleLogout } = props;
+  handleLogout(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/");
+  });
+
+    return (
+    <div>
+    <Header />
+    <Footer />
+    </div>
+    )
+}
+
+export default Logout;
